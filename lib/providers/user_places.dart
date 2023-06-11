eb72319f92074c53afa3e4fa155ddf3bf62cfb4a
Aswin S <aswinofficial3@gmail.com>
2023-06-11 17:37:19 +0530

    Place provider creation completed

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,12 +1,15 @@
-
 import 'package:favorite_places_app/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
-  UserPlacesNotifier(): super(const []);
+  UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title){
-
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
   }
-  
 }
+
+final userPlaceProvider = StateNotifierProvider(
+  (ref) => UserPlacesNotifier(),
+);
