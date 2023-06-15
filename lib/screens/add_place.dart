f510dbb7c7f3fcd42c9952812a957c0239727c5b
Aswin S <aswinofficial3@gmail.com>
2023-06-15 15:32:40 +0530

    Input widget created and added

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places_app/providers/user_places.dart';
+import 'package:favorite_places_app/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,10 +13,10 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
 
-  void _savePlace(){
+  void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if(enteredTitle.isEmpty){
+    if (enteredTitle.isEmpty) {
       return;
     }
     ref.read(userPlaceProvider.notifier).addPlace(enteredTitle);
@@ -45,6 +46,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 10),
+            ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
