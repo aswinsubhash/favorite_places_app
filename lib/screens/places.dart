8397460b080612382487d98b003971c9470181de
Aswin S <aswinofficial3@gmail.com>
2023-07-17 00:39:07 +0530

    Database completed

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,12 +4,25 @@ import 'package:favorite_places_app/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-     final userPlaces = ref.watch(userPlaceProvider);
+  ConsumerState<ConsumerStatefulWidget> createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlaceProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlaceProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -29,9 +42,15 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: userPlaces,
-        ),
+        child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) {
+              return snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CircularProgressIndicator())
+                  : PlacesList(
+                      places: userPlaces,
+                    );
+            }),
       ),
     );
   }
